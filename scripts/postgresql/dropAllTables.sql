780ae26e20f8ee206385019e711f0423a879c7e2
Inazio Claver <jclaver64@gmail.com>
2022-09-21 16:31:43 +0200

    fix postgresql script

--- scripts/postgresql/dropAllTables.sql
+++ scripts/postgresql/dropAllTables.sql
@@ -1,7 +1,7 @@
 -- Recreate the schema
 DROP SCHEMA public CASCADE;
 CREATE SCHEMA public;
-​
+
 -- Restore default permissions
 GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO public;
